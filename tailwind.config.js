6ce3e783f9c9cf395e203ee3e913a20ddd96bc6f
Sebron <sebron@gmail.com>
2024-09-06 02:06:27 -0400

    setup of tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  purge: ["./index.html", "./src/**/*.{vue,js,jsx,tsx}"],
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
