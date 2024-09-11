b4ba5b4a32a37682be8aa813970f0cebda1ec179
Sebron <sebron@gmail.com>
2024-09-11 19:20:50 -0400

    Complete overhaul, now using Vue 3 with typescript support, Vite build tools, and Pinia state management. Also includes ViteTest (Unit Testing) and Playwright support.

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
