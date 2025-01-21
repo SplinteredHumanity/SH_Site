4ad87f9a4ef84107fa20b59c0d207702aa7e52c5
Sebron <sebron@gmail.com>
2025-01-21 06:26:39 -0500

    css cleanup, font antialiasing, etc...

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./index.css";
 import { useState } from "react";
-import Hamburger from "hamburger-react";
+import { Divide as Hamburger } from "hamburger-react";
 
 function App() {
   return (
     <>
-      <div className="max-h-fit flex font-nasa justify-between text-4xl items-center">
+      <div className="max-h-fit flex font-nasa justify-between text-4xl items-center antialiased">
         <div className="text-left pl-5">
           <p>SPLINTERED HUMANITY PRODUCTIONS</p>
         </div>
         <div className="flex text-right hamburger-wrapper items-center pr-3">
           <p>MENU</p>
-          <Hamburger />
+          <Hamburger rounded />
         </div>
       </div>
       <div className="horizontal-rule" />
