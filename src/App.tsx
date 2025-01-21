2c7f2193d47372afbbe46cbe14186264ab4ce38c
Sebron <sebron@gmail.com>
2025-01-21 06:15:13 -0500

    added hamburger-react, messing around with tailwindcss.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./index.css";
+import { useState } from "react";
+import Hamburger from "hamburger-react";
 
 function App() {
   return (
     <>
-      <div className="h-screen flex items-center justify-center">
-        <h1 className="font-nasa text-6xl">SPLINTERED HUMANITY PRODUCTIONS</h1>
+      <div className="max-h-fit flex font-nasa justify-between text-4xl items-center">
+        <div className="text-left pl-5">
+          <p>SPLINTERED HUMANITY PRODUCTIONS</p>
+        </div>
+        <div className="flex text-right hamburger-wrapper items-center pr-3">
+          <p>MENU</p>
+          <Hamburger />
+        </div>
       </div>
+      <div className="horizontal-rule" />
     </>
   );
 }
