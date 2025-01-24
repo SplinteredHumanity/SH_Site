666180577d82722e579260bc44762a820ac4ec05
Sebron <sebron@gmail.com>
2025-01-24 01:37:03 -0500

    moved header to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import "./index.css";
-import { useState } from "react";
-import { Divide as Hamburger } from "hamburger-react";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <>
-      <div className="max-h-fit flex font-nasa justify-between text-4xl items-center antialiased">
-        <div className="text-left pl-5">
-          <p>SPLINTERED HUMANITY PRODUCTIONS</p>
-        </div>
-        <div className="flex text-right hamburger-wrapper items-center pr-3">
-          <p>MENU</p>
-          <Hamburger rounded />
-        </div>
-      </div>
-      <div className="horizontal-rule" />
+      <Header />
     </>
   );
 }
