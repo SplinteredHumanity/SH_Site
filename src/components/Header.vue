dc9635329444b9054c89880b374db9cfb9d04e43
Sebron <sebron@gmail.com>
2025-02-04 22:00:15 -0500

    Adding Header back, no fix for burger animations in Vue yet.

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div className="flex">
+    <p className="pl-5 shrink-0 text-left text-4xl font-nasa">
+      Splintered Humanity
+    </p>
+    <p className="pr-5 grow text-right text-4xl font-nasa">Menu</p>
+  </div>
+  <div className="horizontal-rule" />
+</template>
